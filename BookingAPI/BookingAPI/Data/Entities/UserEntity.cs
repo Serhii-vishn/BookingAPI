da3696a4ea7f43e106a27fc9dbcca8d139214c32
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-05-10 23:16:56 +0300

    implemeted Entities

---
+++ BookingAPI/BookingAPI/Data/Entities/UserEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace BookingAPI.Data.Entities
+{
+    public class UserEntity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public DateOnly DateOfBirth { get; set; }
+        public string Gender { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? PictureFileName { get; set; }
+
+        public IList<BookingEntity> Bookings { get; set; } = new List<BookingEntity>();
+    }
+}
---
+++ BookingAPI/BookingAPI/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using Microsoft.EntityFrameworkCore;
+
+global using BookingAPI.Data;
+global using BookingAPI.Data.Entities;
+global using BookingAPI.Data.EntityConfigurations;
