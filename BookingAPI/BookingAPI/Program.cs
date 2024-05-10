a6e0246d9827b5642ed9a087722703bb38b20711
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-05-10 22:28:10 +0300

    project setup

--- BookingAPI/BookingAPI/Program.cs
+++ BookingAPI/BookingAPI/Program.cs
@@ -1,15 +1,13 @@
+var configuration = GetConfiguration();
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -23,3 +21,13 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+IConfiguration GetConfiguration()
+{
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables();
+
+    return builder.Build();
+}
