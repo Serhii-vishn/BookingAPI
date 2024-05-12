023060ffac6cd9f52282058d0de55d13b1e98b58
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-05-13 02:12:33 +0300

    added booking dto, requests

---
+++ BookingAPI/BookingAPI/Models/Requests/UpdateBookingRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace BookingAPI.Models.Requests
+{
+    public class UpdateBookingRequest
+    {
+        public int Id { get; set; }
+        public DateOnly DateStart { get; set; }
+        public DateOnly DateEnd { get; set; }
+
+        public int ClientId { get; set; }
+        public int AccommodationId { get; set; }
+    }
+}
